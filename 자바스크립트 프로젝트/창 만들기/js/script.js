fdb1a49ff4fc8c9dfd01edce6eaaf42199c8e00c
huhonmcmc <huhonmcmc@gmail.com>
2024-08-13 11:36:55 +0900

    폴더 삭제

--- 자바스크립트 프로젝트/창 만들기/js/script.js
+++ 자바스크립트 프로젝트/창 만들기/js/script.js
@@ -37,3 +37,27 @@ addBox.addEventListener("mousedown", (e) => {
 addBox.ondragstart = function() {
     return false;
 }
+
+let word = document.getElementById('word');
+let meaning = document.getElementById('meaning');
+let addWord = document.getElementById('inputWord'); 
+if(addWord.textContent=='')
+    addWord.innerHTML = "아직 추가된 단어가 업습니다.";
+
+
+document.getElementById('addBu').addEventListener('click', () => {
+    if(word.value=='')
+        alert("단어를 입력해주세요!");
+    else if(meaning.value=='')
+        alert("뜻을 입력해주세요!");
+    else{
+        if(addWord.textContent == "아직 추가된 단어가 업습니다.")
+            addWord.innerHTML = "단어명 : " + word.value + " 뜻 : " + meaning.value + "<br>";
+        else
+            addWord.innerHTML += "단어명 : " + word.value + " 뜻 : " + meaning.value + "<br>";
+    }
+});
+document.getElementById('canBu').addEventListener('click', () => {
+    word.value = '';
+    meaning.value = '';
+});
