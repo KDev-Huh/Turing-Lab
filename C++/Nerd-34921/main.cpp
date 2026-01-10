61f35f0d79559f3a38b29e20ceacfee3f3be5000
huhonmcmc <huhonmcmc@gmail.com>
2026-01-10 22:25:58 +0900

    34921 덕후 :: 백준 문제 풀이

---
+++ C++/Nerd-34921/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, t;
+    
+    cin >> a >> t;
+    
+    int p = 10 + 2 * (25 - a + t);
+    
+    cout << (p > 0 ? p : 0);
+
+    return 0;
+}
