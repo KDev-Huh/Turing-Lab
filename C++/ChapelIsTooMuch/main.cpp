a76f7c7a05e53545043454b094020afe205c5d92
huhonmcmc <huhonmcmc@gmail.com>
2026-01-12 21:49:58 +0900

    34945 채플이 너무해 :: 백준 문제 풀이

---
+++ C++/ChapelIsTooMuch/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    
+    cin >> x;
+    
+    if(x >= 6) cout << "Success!";
+    else cout << "Oh My God!";
+
+    return 0;
+}
